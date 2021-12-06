870e4ae8348d95b4fd9230bfa5ec526b5c420dbc
Douglas DuBois <54450869+douglasdubois@users.noreply.github.com>
2021-12-06 10:44:19 -0600

    added regex to remove key

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import re
 import requests
 
-filename = 'example.txt'
+filename = 'input.txt'
 
 def unshorten_url(url):
         response = requests.head(url, allow_redirects=True)
         return response.url
 
+url_re = re.compile(r'(https?:\/{2})?.*goo.gl\/\w*')
+key_re = re.compile(r'\?key=\w*')
+
 with open(filename, 'r+') as f:
         content = f.read()
-        content = re.sub('(https?:\/{2})?.*goo.gl\/\w*', lambda x: unshorten_url(x.group()), content)
+        content = url_re.sub(lambda x: unshorten_url(x.group()), content)
+        content = key_re.sub('', content)
         f.seek(0)
+        f.truncate(0)
         f.write(content)
-        f.close()
